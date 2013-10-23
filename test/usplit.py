9a0173046ff459e21fe4f45ef6ae2f70a99fe131
Neuron Teckid <Lene13@gmail.com>
2013-10-23 17:53:58 +0800

    refactor usplit test

--- test/usplit.py
+++ test/usplit.py
@@ -6,142 +6,81 @@ import unicodeblock.sequence
 
 
 class UnicodeSplit(unittest.TestCase):
+    def _assert_sequence(self, s, expected_split):
+        seqs = unicodeblock.sequence.usplit(s)
+        self.assertEqual(len(expected_split), len(seqs))
+        for i, seq in enumerate(seqs):
+            self.assertEqual(expected_split[i][0], seq.lang)
+            self.assertEqual(expected_split[i][1], seq.value)
+
     def test_usplit(self):
-        seqs = unicodeblock.sequence.usplit(u'`12345t6y7u8iop[-09')
-        self.assertEqual(9, len(seqs))
-        seq = seqs[0]
-        self.assertEqual('12345', seq.value)
-        self.assertEqual('digit', seq.lang)
-        seq = seqs[1]
-        self.assertEqual('t', seq.value)
-        self.assertEqual('en', seq.lang)
-        seq = seqs[2]
-        self.assertEqual('6', seq.value)
-        self.assertEqual('digit', seq.lang)
-        seq = seqs[3]
-        self.assertEqual('y', seq.value)
-        self.assertEqual('en', seq.lang)
-        seq = seqs[4]
-        self.assertEqual('7', seq.value)
-        self.assertEqual('digit', seq.lang)
-        seq = seqs[5]
-        self.assertEqual('u', seq.value)
-        self.assertEqual('en', seq.lang)
-        seq = seqs[6]
-        self.assertEqual('8', seq.value)
-        self.assertEqual('digit', seq.lang)
-        seq = seqs[7]
-        self.assertEqual('iop', seq.value)
-        self.assertEqual('en', seq.lang)
-        seq = seqs[8]
-        self.assertEqual('09', seq.value)
-        self.assertEqual('digit', seq.lang)
+        self._assert_sequence(u'`12345t6y7u8iop[-09', (
+            ('digit', '12345'),
+            ('en', 't'),
+            ('digit', '6'),
+            ('en', 'y'),
+            ('digit', '7'),
+            ('en', 'u'),
+            ('digit', '8'),
+            ('en', 'iop'),
+            ('digit', '09'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(u'unicodeblock.sequence.usplit')
-        self.assertEqual(3, len(seqs))
-        seq = seqs[0]
-        self.assertEqual('unicodeblock', seq.value)
-        self.assertEqual('en', seq.lang)
-        seq = seqs[1]
-        self.assertEqual('sequence', seq.value)
-        self.assertEqual('en', seq.lang)
-        seq = seqs[2]
-        self.assertEqual('usplit', seq.value)
-        self.assertEqual('en', seq.lang)
+        self._assert_sequence(u'unicodeblock.sequence.usplit', (
+            ('en', 'unicodeblock'),
+            ('en', 'sequence'),
+            ('en', 'usplit'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(u"Kuroko's Basketball")
-        self.assertEqual(2, len(seqs))
-        seq = seqs[0]
-        self.assertEqual("Kuroko's", seq.value)
-        self.assertEqual('en', seq.lang)
-        seq = seqs[1]
-        self.assertEqual(u'Basketball', seq.value)
-        self.assertEqual(u'en', seq.lang)
+        self._assert_sequence(u"Kuroko's Basketball", (
+            ('en', "Kuroko's"),
+            ('en', 'Basketball'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(
-            u'Kiss-Shot Acerola-Orion Heart-Under-Blade')
-        self.assertEqual(3, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'Kiss-Shot', seq.value)
-        self.assertEqual(u'en', seq.lang)
-        seq = seqs[1]
-        self.assertEqual(u'Acerola-Orion', seq.value)
-        self.assertEqual(u'en', seq.lang)
-        seq = seqs[2]
-        self.assertEqual(u'Heart-Under-Blade', seq.value)
-        self.assertEqual(u'en', seq.lang)
+        self._assert_sequence('Kiss-Shot Acerola-Orion Heart-Under-Blade', (
+            ('en', 'Kiss-Shot'),
+            ('en', 'Acerola-Orion'),
+            ('en', 'Heart-Under-Blade'),
+        ))
 
     def test_latin_mixed(self):
-        seqs = unicodeblock.sequence.usplit(u'Diomedéa')
-        self.assertEqual(1, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'Diomedéa', seq.value)
-        self.assertEqual(u'latin', seq.lang)
+        self._assert_sequence(u'Diomedéa', (
+            ('latin', u'Diomedéa'),
+        ))
 
     def test_cjk_mixed(self):
-        seqs = unicodeblock.sequence.usplit(u'らき☆すた')
-        self.assertEqual(2, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'らき', seq.value)
-        self.assertEqual(u'ja', seq.lang)
-        seq = seqs[1]
-        self.assertEqual(u'すた', seq.value)
-        self.assertEqual(u'ja', seq.lang)
+        self._assert_sequence(u'らき☆すた', (
+            ('ja', u'らき'),
+            ('ja', u'すた'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(
-            u'うたの☆プリンスさまっ♪ マジLOVE2000%')
-        self.assertEqual(5, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'うたの', seq.value)
-        self.assertEqual(u'ja', seq.lang)
-        seq = seqs[1]
-        self.assertEqual(u'プリンスさまっ', seq.value)
-        self.assertEqual(u'ja', seq.lang)
-        seq = seqs[2]
-        self.assertEqual(u'マジ', seq.value)
-        self.assertEqual(u'ja', seq.lang)
-        seq = seqs[3]
-        self.assertEqual(u'LOVE', seq.value)
-        self.assertEqual(u'en', seq.lang)
-        seq = seqs[4]
-        self.assertEqual(u'2000', seq.value)
-        self.assertEqual(u'digit', seq.lang)
+        self._assert_sequence(u'うたの☆プリンスさまっ♪ マジLOVE2000%', (
+            ('ja', u'うたの'),
+            ('ja', u'プリンスさまっ'),
+            ('ja', u'マジ'),
+            ('en', 'LOVE'),
+            ('digit', '2000'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(u'荻上千佳')
-        self.assertEqual(1, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'荻上千佳', seq.value)
-        self.assertEqual(u'cjk', seq.lang)
+        self._assert_sequence(u'荻上千佳', (
+            ('cjk', u'荻上千佳'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(u'涼宮ハルヒの憂鬱2006')
-        self.assertEqual(2, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'涼宮ハルヒの憂鬱', seq.value)
-        self.assertEqual(u'ja', seq.lang)
-        seq = seqs[1]
-        self.assertEqual(u'2006', seq.value)
-        self.assertEqual(u'digit', seq.lang)
+        self._assert_sequence(u'涼宮ハルヒの憂鬱2006', (
+            ('ja', u'涼宮ハルヒの憂鬱'),
+            ('digit', '2006'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(u'涼宮 ハルヒ')
-        self.assertEqual(2, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'涼宮', seq.value)
-        self.assertEqual(u'cjk', seq.lang)
-        seq = seqs[1]
-        self.assertEqual(u'ハルヒ', seq.value)
-        self.assertEqual(u'ja', seq.lang)
+        self._assert_sequence(u'涼宮 ハルヒ', (
+            ('cjk', u'涼宮'),
+            ('ja', u'ハルヒ'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(u'カードキャプターさくら')
-        self.assertEqual(1, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'カードキャプターさくら', seq.value)
-        self.assertEqual(u'ja', seq.lang)
+        self._assert_sequence(u'カードキャプターさくら', (
+            ('ja', u'カードキャプターさくら'),
+        ))
 
-        seqs = unicodeblock.sequence.usplit(u'丹下桜단게사쿠라')
-        self.assertEqual(2, len(seqs))
-        seq = seqs[0]
-        self.assertEqual(u'丹下桜', seq.value)
-        self.assertEqual(u'cjk', seq.lang)
-        seq = seqs[1]
-        self.assertEqual(u'단게사쿠라', seq.value)
-        self.assertEqual(u'kr', seq.lang)
+        self._assert_sequence(u'丹下桜단게사쿠라', (
+            ('cjk', u'丹下桜'),
+            ('kr', u'단게사쿠라'),
+        ))
