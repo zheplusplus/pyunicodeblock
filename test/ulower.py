f68b326c14fb6776fbc98da67193da0b1906acc2
Zhe Lin <zhe.lin@localfolio.co.jp>
2019-12-27 11:45:54 +0900

    Switch to Python3

--- test/ulower.py
+++ test/ulower.py
@@ -8,34 +8,34 @@ import unicodeblock.lower
 class UniversalLower(unittest.TestCase):
     def test_lower_kana(self):
         self.assertEqual(
-            u'ミカサ・アッカーマン',
-            unicodeblock.lower.lower_kanas(u'ミカサ・アッカーマン'))
+            'ミカサ・アッカーマン',
+            unicodeblock.lower.lower_kanas('ミカサ・アッカーマン'))
         self.assertEqual(
-            u'ノビ太ノクセニ',
-            unicodeblock.lower.lower_kanas(u'のび太のくせに'))
+            'ノビ太ノクセニ',
+            unicodeblock.lower.lower_kanas('のび太のくせに'))
         self.assertEqual(
-            u'ノビ太ノクセニ',
-            unicodeblock.lower.lower_kanas(u'のび太のクセに'))
+            'ノビ太ノクセニ',
+            unicodeblock.lower.lower_kanas('のび太のクセに'))
 
     def test_fullwidth_letters(self):
-        self.assertEqual(u'the quick brown fox jumps over the lazy dog.',
+        self.assertEqual('the quick brown fox jumps over the lazy dog.',
                          unicodeblock.lower.lower_fullwidths(
-                             u'Ｔｈｅ ｑｕｉｃｋ ｂｒｏｗｎ ｆｏｘ ' +
-                             u'ｊｕｍｐｓ ｏｖｅｒ ｔｈｅ ｌａｚｙ ｄｏｇ．'))
+                             'Ｔｈｅ ｑｕｉｃｋ ｂｒｏｗｎ ｆｏｘ ' +
+                             'ｊｕｍｐｓ ｏｖｅｒ ｔｈｅ ｌａｚｙ ｄｏｇ．'))
 
     def test_ulower(self):
         self.assertEqual(
-            u'ノビ太ノクセニ',
-            unicodeblock.lower.ulower(u'のび太のクセに'))
-        self.assertEqual(u'the quick brown fox jumps over the lazy dog.',
+            'ノビ太ノクセニ',
+            unicodeblock.lower.ulower('のび太のクセに'))
+        self.assertEqual('the quick brown fox jumps over the lazy dog.',
                          unicodeblock.lower.ulower(
-                             u'Ｔｈｅ ｑｕｉｃｋ ｂｒｏｗｎ ｆｏｘ ' +
-                             u'ｊｕｍｐｓ ｏｖｅｒ ｔｈｅ ｌａｚｙ ｄｏｇ．'))
-        self.assertEqual(u'ノビ太ノクセニ ' +
-                         u'the quick brown fox jumps over the lazy dog. ' +
-                         u"the browns' kitsune",
+                             'Ｔｈｅ ｑｕｉｃｋ ｂｒｏｗｎ ｆｏｘ ' +
+                             'ｊｕｍｐｓ ｏｖｅｒ ｔｈｅ ｌａｚｙ ｄｏｇ．'))
+        self.assertEqual('ノビ太ノクセニ ' +
+                         'the quick brown fox jumps over the lazy dog. ' +
+                         "the browns' kitsune",
                          unicodeblock.lower.ulower(
-                             u'のび太のクセに ' +
-                             u'Ｔｈｅ ｑｕｉｃｋ ｂｒｏｗｎ ｆｏｘ ' +
-                             u'ｊｕｍｐｓ ｏｖｅｒ ｔｈｅ ｌａｚｙ ｄｏｇ． ' +
-                             u"The Browns' kitsune"))
+                             'のび太のクセに ' +
+                             'Ｔｈｅ ｑｕｉｃｋ ｂｒｏｗｎ ｆｏｘ ' +
+                             'ｊｕｍｐｓ ｏｖｅｒ ｔｈｅ ｌａｚｙ ｄｏｇ． ' +
+                             "The Browns' kitsune"))
